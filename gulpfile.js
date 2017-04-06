df66ef65417175f91280cfed682d1dc80d323340
Ben Carr <benjamin.carr@sky.uk>
2017-04-06 20:06:27 +0100

    Fix JS gulp task and add sass gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
+var sass = require('gulp-sass');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
 
 gulp.task('js', function() {
   var bundleStream = browserify('_assets/main.js').bundle();
   return bundleStream
     .pipe(source('main.js'))
-    .pipe(uglify())
+    .pipe(streamify(uglify()))
     .pipe(rename('bundle.js'))
     .pipe(gulp.dest('./assets'))
 });
 
-gulp.task('default', ['js']);
+gulp.task('css', function() {
+  return gulp.src('_assets/main.scss')
+    .pipe(sass({ outputStyle: 'compressed' })
+      .on('error', sass.logError))
+    .pipe(gulp.dest('assets'));
+});
+
+gulp.task('default', ['js', 'css']);
