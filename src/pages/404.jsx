cf2492da044105c3a3a392c093259d1ecbe872b7
Ben Carr <benjamin.carr@sky.uk>
2018-06-02 00:33:50 +0100

    Add 404 and thanks pages

---
+++ src/pages/404.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+export default () => (
+  <section id="not-found" className="container container-flush">
+    <div className="center-all full-height">
+      <div className="center-all-inner">
+        <h1 className="status-code">404</h1>
+        <p className="message">Looks like you&#8217;re lost!</p>
+        <Link to="/">Back home</Link>
+      </div>
+    </div>
+  </section>
+);
---
+++ src/pages/thanks.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+export default () => (
+  <section id="thanks" className="container container-flush">
+    <div className="center-all full-height">
+      <div className="center-all-inner">
+        <h1 className="title">Thanks for your message!</h1>
+        <Link to="/">Back home</Link>
+      </div>
+    </div>
+  </section>
+);
