c2d4ebf77e62d7d1d2a834ab1953557e101caa84
Ben Carr <benjamin.carr@sky.uk>
2018-07-16 21:39:46 +0100

    Create breadcrumbs component

--- src/pages/changelog.jsx
+++ src/pages/changelog.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Changelog from '../components/changelog';
 import Footer from '../components/footer/footer';
+import Breadcrumbs from '../components/breadcrumbs';
+
+const breadcrumbs = [{ name: 'Changelog' }];
 
 export default props => {
   const { footer, changelog } = props.data.allDataJson.edges[0].node;
   return (
     <div id="changelog">
       <div className="container">
+        <Breadcrumbs crumbs={breadcrumbs} />
         <Changelog items={changelog} />
       </div>
       <Footer {...footer} changelog={changelog} />
