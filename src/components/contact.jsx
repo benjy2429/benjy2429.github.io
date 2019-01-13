f5c2fec149a9ac6be8113c7ac63d1364585aa02b
Ben Carr <benjamin.carr@sky.uk>
2019-01-13 15:25:37 +0000

    Add custom validation for the contact form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,39 +1,178 @@
 import React from 'react';
 
-export default () => (
-  <section className="contact">
-    <div className="container">
-      <h2 id="contact">Contact</h2>
-      <p>
-        I hope I&#8217;ve covered everything but if you have any questions or
-        comments, drop me a message.
-      </p>
+const FORM_ENDPOINT = 'https://www.enformed.io/6b8u88yv';
+const REDIRECT_PATH = '/thanks';
+const ERROR_PATH = '/formerror';
 
-      <form action="https://www.enformed.io/6b8u88yv" method="POST">
-        <input
-          type="hidden"
-          name="*redirect"
-          value="https://www.bencarr.co.uk/thanks"
-        />
-        <input
-          type="hidden"
-          name="*subject"
-          value="bencarr.co.uk Contact Form"
-        />
-        <input type="hidden" name="*reply" value="email" />
-        <input type="hidden" name="*honeypot" />
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <label htmlFor="contact-name">Name</label>
-        <input type="text" name="name" id="contact-name" required />
+    this.state = {
+      name: {
+        value: '',
+        errors: []
+      },
+      email: {
+        value: '',
+        errors: []
+      },
+      message: {
+        value: '',
+        errors: []
+      },
+      honeypot: {
+        value: '',
+        errors: []
+      }
+    };
 
-        <label htmlFor="contact-email">Email</label>
-        <input type="email" name="email" id="contact-email" required />
+    this.submitForm = this.submitForm.bind(this);
+    this.updateFormData = this.updateFormData.bind(this);
+    this.validateFormData = this.validateFormData.bind(this);
+  }
 
-        <label htmlFor="contact-message">Message</label>
-        <textarea name="message" id="contact-message" required />
+  get hasValidationErrors() {
+    return Object.keys(this.state).reduce((isInvalid, key) => {
+      const { errors } = this.state[key];
+      return isInvalid || errors.length;
+    }, false);
+  }
 
-        <input type="submit" value="Send" />
-      </form>
-    </div>
-  </section>
-);
+  validateFormData() {
+    const newState = this.state;
+    Object.keys(newState).forEach(key => {
+      newState[key].errors = [];
+    });
+
+    if (this.state.honeypot.value.length) {
+      newState.honeypot.errors.push('Honeypot error');
+    }
+
+    ['name', 'email', 'message'].forEach(target => {
+      if (!this.state[target].value.length) {
+        newState[target].errors.push(`Please enter a valid ${target}`);
+      }
+    });
+
+    if (/http/g.test(this.state.message.value)) {
+      newState.message.errors.push(
+        'Please do not include links in your message'
+      );
+    }
+
+    this.setState(newState);
+  }
+
+  updateFormData(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: {
+        ...this.state[name],
+        value
+      }
+    });
+  }
+
+  async submitForm(event) {
+    event.preventDefault();
+
+    this.validateFormData();
+
+    if (this.hasValidationErrors) {
+      return;
+    }
+
+    const formData = {
+      '*subject': 'bencarr.co.uk Contact Form',
+      '*reply': this.state.email.value,
+      '*honeypot': this.state.honeypot.value,
+      name: this.state.name.value,
+      email: this.state.email.value,
+      message: this.state.message.value
+    };
+
+    try {
+      const response = await fetch(FORM_ENDPOINT, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const { status } = await response.json();
+
+      if (status === 200) {
+        window.location.href = status === 200 ? REDIRECT_PATH : ERROR_PATH;
+      }
+    } catch (e) {
+      window.location.href = ERROR_PATH;
+    }
+  }
+
+  render() {
+    const { name, email, message } = this.state;
+
+    return (
+      <section className="contact">
+        <div className="container">
+          <h2 id="contact">Contact</h2>
+          <p>
+            I hope I&#8217;ve covered everything but if you have any questions
+            or comments, drop me a message.
+          </p>
+
+          <form onSubmit={this.submitForm}>
+            <input type="hidden" name="*reply" value="email" />
+            <input
+              type="hidden"
+              name="honeypot"
+              onChange={this.updateFormData}
+            />
+
+            <label htmlFor="contact-name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="contact-name"
+              onChange={this.updateFormData}
+              className={name.errors.length > 0 ? 'invalid' : ''}
+              required
+            />
+            {name.errors.length > 0 && (
+              <div className="validation-error">{name.errors[0]}</div>
+            )}
+
+            <label htmlFor="contact-email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="contact-email"
+              onChange={this.updateFormData}
+              className={email.errors.length > 0 ? 'invalid' : ''}
+              required
+            />
+            {email.errors.length > 0 && (
+              <div className="validation-error">{email.errors[0]}</div>
+            )}
+
+            <label htmlFor="contact-message">Message</label>
+            <textarea
+              name="message"
+              id="contact-message"
+              onChange={this.updateFormData}
+              className={message.errors.length > 0 ? 'invalid' : ''}
+              required
+            />
+            {message.errors.length > 0 && (
+              <div className="validation-error">{message.errors[0]}</div>
+            )}
+
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      </section>
+    );
+  }
+}
